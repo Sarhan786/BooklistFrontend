8bd8b8cfbed96ba8d8856f5d7b56e9760a1aa3d6
Sarhan786 <sarahan786@gmail.com>
2022-12-03 13:02:42 +0530

    Add some inegration from backend

--- booklist/src/Componentes/Booklist/Booklist.js
+++ booklist/src/Componentes/Booklist/Booklist.js
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Booklist() {
     const[Books, setBooks] = useState([]);
     
-    useEffect(()=>{},[]);
+    useEffect(()=>{
+      axios.get("http://localhost:5050/booklist/")
+      .then((res)=>{
+        console.log(res.data.BooksData);
+        setBooks(res.data.BooksData)
+      })
+    },[]);
 
   return (
     <div className="Booklist">
@@ -16,7 +23,14 @@ function Booklist() {
       <button>AddBook</button>
       </Link>
       <div className="BooksContainer">
-        {Books.map(()=>(<></>))}
+        {Books.map((book)=>(<div className="Book">
+          <div className="Imgboog"></div>
+          <div className="Details">
+            <h4>{book.title}</h4>
+            <h4>{book.author}</h4>
+            <h4>{book.description}</h4>
+          </div>
+        </div>))}
       </div>
     </div>
   );
